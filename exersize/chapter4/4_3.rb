fcad1f3ae9d9039183a1f21c8156825554ffbb93
satoshitodaka <satoshitodaka0705@gmail.com>
2021-08-28 17:29:34 +0900

    finish chapter4 exercize

---
+++ exersize/chapter4/4_3.rb
@@ -0,0 +1,10 @@
+drinks = ['コーヒー', 'カフェラテ']
+drinks.push 'モカ'
+p drinks
+
+x = [2, 3]
+p x.unshift(1)
+
+x = [1, 2]
+y = [3, 4]
+p x + y
---
+++ exersize/chapter4/4_4.rb
@@ -0,0 +1,11 @@
+drinks = ['ティーラテ', 'カフェラテ', '抹茶ラテ']
+puts drinks
+drinks.each do |drink|
+  puts "#{drink}お願いします"
+end
+
+sum = 0
+[1, 2, 3].each do |number|
+  sum += number
+end
+puts sum
