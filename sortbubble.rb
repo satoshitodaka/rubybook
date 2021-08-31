9eddd9450f6914567837d311943f7616833a7e5d
satoshitodaka <satoshitodaka0705@gmail.com>
2021-09-01 08:04:24 +0900

    要素の入れ替え処理を簡素化

--- sortbubble.rb
+++ sortbubble.rb
@@ -1,15 +1,12 @@
 numbers = [3, 1, 2, 5, 11, 4, 10, 7, 8, 6]
 counts = numbers.size - 1
 loop do
-  # 処理前の配列の複製を保存
+  # 処理前の配列の複製を作成
   before_process = numbers.dup
   n = 0
   counts.times do
     if numbers[n] > numbers[n + 1]
-      left_number = numbers[n]
-      right_number = numbers[n + 1]
-      numbers[n] = right_number
-      numbers[n + 1] = left_number
+      numbers[n], numbers[n + 1] = numbers[n + 1], numbers[n]
     end
     n += 1
   end
